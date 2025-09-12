f6a979b24667da076f26611d19e7dc22435da571
Hussain Ahmed <84622365+hussainmohd03@users.noreply.github.com>
2025-09-12 14:56:33 +0300

    make oracle logo a reusable component

---
+++ src/components/OracleLogo.jsx
@@ -0,0 +1,11 @@
+const OracleLogo = () => {
+  return (
+    <>
+      <section className="landing-page-logo">
+        <img src="logo.svg" alt="OHP Logo" />
+      </section>
+    </>
+  )
+}
+
+export default OracleLogo
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,24 +1,28 @@
 import { useNavigate } from 'react-router-dom'
+import OracleLogo from '../components/OracleLogo'
 
 const Landing = () => {
   return (
     <>
       <main className="landing-page-main">
-        <section className="landing-page-logo">
-          <img src="logo.svg" alt="OHP Logo" />
-        </section>
+        <OracleLogo />
         <section className="landing-page-content">
           <h2>Oracle Health</h2>
           <h1>Possibilities</h1>
           <p>
             Reimagine the future of health with an automated, secure machine
-            learning platform fostering the greatness of accuracy{' '}
+            learning platform fostering the greatness of accuracy
           </p>
           <button>Explore OHP</button>
         </section>
-        <section className="landing-page-img">
-          <img src="landing-side-img.svg" alt="" />
+        <section className="landing-page-right-img">
+          <img
+            src="landing-side-img.svg"
+            alt="Landing page Lady Image"
+            id="landing-lady-img"
+          />
         </section>
+        <img src="oracle-logo.svg" alt="Oracle Logo" id="oracle-logo" />
       </main>
     </>
   )
