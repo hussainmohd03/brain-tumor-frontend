1d1a3a63062c5836634707c491c59d4b68ad70bc
Hussain Ahmed <84622365+hussainmohd03@users.noreply.github.com>
2025-09-13 00:33:02 +0300

    complete signup style step one

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,6 +7,26 @@ const Signup = () => {
         <section className="page-content signup-content">
           <h2 id="signup-title">Get client intuitive clinical experiences </h2>
           <h1 id="signup-header">Sign up</h1>
+          <form action="">
+            <div className="input-field">
+              <label htmlFor="fullName">Full Name*</label>
+              <input type="text" />
+            </div>
+            <div className="input-field">
+              <label htmlFor="email">Email*</label>
+              <input type="email" />
+            </div>
+            <div className="input-field">
+              <label htmlFor="institution">Institution*</label>
+              <input type="text" />
+            </div>
+            <div className="input-field">
+              <label htmlFor="NHRA">NHRA License*</label>
+              <input type="text" minLength={8} />
+            </div>
+            <button type="submit" className='signup-button'>Continue</button>
+          </form>
+            <p id='login-prompt'>Already have an account? <a href="/login">Login</a></p>
         </section>
 
         <img src="oracle-logo.svg" alt="Oracle Logo" id="oracle-logo" />
