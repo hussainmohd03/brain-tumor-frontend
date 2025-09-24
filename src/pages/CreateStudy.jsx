e27efd85a515fd8fd7aa6a38efc3cce85df10e7a
Hussain Ahmed <84622365+hussainmohd03@users.noreply.github.com>
2025-09-24 11:21:43 +0300

    complete create-study starting page

--- src/pages/CreateStudy.jsx
+++ src/pages/CreateStudy.jsx
@@ -6,7 +6,27 @@ const CreateStudy = () => {
       <main className="main-container">
         <Header />
         <Sidebar />
-        <section className="create-study-section"></section>
+        <section className="create-study-section">
+          <div className="starting">
+            <img src="/images/bar.svg" alt="" id="bar-img" />
+            <div>
+              <h1 className="create-study-title">
+                Intelligence––like never before.
+              </h1>
+              <div className="image-container">
+                <div className="image-overlay">
+                  <h3>Oracle Health Possibilities</h3>
+                  <p>
+                    You can now process your local scans for precise and
+                    detailed discoveries. Explore the exclusive offerings of
+                    OHP.
+                  </p>
+                  <button className='uncover-button'>Uncover a new world</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
         <img src="/logos/oracle-logo.svg" alt="Oracle Logo" id="oracle-logo" />
       </main>
     </>
